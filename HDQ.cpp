ae306813042842a9cacd215dfec47fc52c938e02
nbagaev <pomojka@online.de>
2020-09-16 16:41:44 +0200

    Update HDQ.cpp

--- HDQ.cpp
+++ HDQ.cpp
@@ -73,7 +73,8 @@ extern "C" {
 * @param pinArg: pin number to attach to 
 *
 */ 
-HDQ::HDQ(uint8_t pinArg) { 
+HDQ::HDQ(uint8_t pinArg)
+{ 
 	pin = pinArg; 
 	port = digitalPinToPort(pin); 
 	bitmask = digitalPinToBitMask(pin); 
@@ -86,7 +87,8 @@ HDQ::HDQ(uint8_t pinArg) {
 * 
 * sendBreak: writes a break to the HDQ line 
 */ 
-void HDQ::doBreak(void) { 
+void HDQ::doBreak(void)
+{ 
 	sbi(*modeReg, pin); // Set pin as output
 
 	// Singal a break on the line
@@ -96,46 +98,45 @@ void HDQ::doBreak(void) {
 	// Make sure we leave enough time for the slave to recover
 	cbi(*modeReg, pin);     // Release pin
 	delayMicroseconds(HDQ_DELAY_TBR);
-
 }
 
 /*
  writeByte: write a raw byte of data to the bus 
  @param payload: the byte to send 
 */ 
-void HDQ::writeByte(uint8_t payload) {
-sbi(*modeReg, pin); // Set pin as output
+void HDQ::writeByte(uint8_t payload)
+{
+	sbi(*modeReg, pin); // Set pin as output
 
-for (uint8_t ii = 0; ii < 8; ii++) {
-
-    // Start bit
-    cbi(*outputReg, pin);   // Bring pin low
-    delayMicroseconds(HDQ_DELAY_THW1);
+	for (uint8_t ii = 0; ii < 8; ii++)
+	{
+		// Start bit
+		cbi(*outputReg, pin);   // Bring pin low
+		delayMicroseconds(HDQ_DELAY_THW1);
 	
-    // Toggle the pin for this bit
-    if (payload>>ii & 0x01) {	// LSB First
-        sbi(*outputReg, pin); // High
-    }
-    else {
-        cbi(*outputReg, pin); // Low
-    }
+		// Toggle the pin for this bit
+		if (payload>>ii & 0x01)
+		{	// LSB First
+			sbi(*outputReg, pin); // High
+		}
+		else
+		{
+			cbi(*outputReg, pin); // Low
+		}
 
-    // Bit time
-    delayMicroseconds(HDQ_DELAY_THW0 - HDQ_DELAY_THW1 + 5);
+		// Bit time
+		delayMicroseconds(HDQ_DELAY_THW0 - HDQ_DELAY_THW1 + 5);
 
-	// Stop bit
-    sbi(*outputReg, pin);   // Bring the pin high
-	delayMicroseconds(HDQ_DELAY_TCYCH - HDQ_DELAY_THW0);
-		
-}
+		// Stop bit
+		sbi(*outputReg, pin);   // Bring the pin high
+		if(ii<7)
+		delayMicroseconds(HDQ_DELAY_TCYCH - HDQ_DELAY_THW0);		
+	}
 
-// Make sure we leave enough time for the slave to recover
-cbi(*modeReg, pin);     // Release pin
-//delayMicroseconds(HDQ_DELAY_TBR);
-delayMicroseconds(HDQ_DELAY_TCYCH - HDQ_DELAY_THW0);
-
-return;
-
+	// Make sure we leave enough time for the slave to recover
+	cbi(*modeReg, pin);     // Release pin
+	//delayMicroseconds(HDQ_DELAY_TBR);
+	//delayMicroseconds(HDQ_DELAY_TCYCH - HDQ_DELAY_THW0);
 }
 
 
@@ -150,26 +151,24 @@ return;
 * return true if it matches the payload 
 *
 */ 
-bool HDQ::write(uint8_t reg, uint8_t payload) { 
-// Singal a break 
-HDQ::doBreak();
+void HDQ::write(uint8_t reg, uint8_t payload)
+{ 
+	// Singal a break 
+	HDQ::doBreak();
 
-// Write the register to write
-HDQ::writeByte((reg |= HDQ_ADDR_MASK_WRITE));
+	// Write the register to write
+	HDQ::writeByte((reg |= HDQ_ADDR_MASK_WRITE));
 
-// Wait for the slave to finish reading the register
-delayMicroseconds((HDQ_DELAY_TRSPS_MAX - HDQ_DELAY_BIT_TOTAL) / 2);
+	// Wait for the slave to finish reading the register
+	delayMicroseconds((HDQ_DELAY_TRSPS_MAX - HDQ_DELAY_BIT_TOTAL) / 2);
 
-// Write the payload
-HDQ::writeByte(payload);
+	// Write the payload
+	HDQ::writeByte(payload);
 
-// Wait for the slave to finish writing the payload
-delayMicroseconds((HDQ_DELAY_TRSPS_MAX - HDQ_DELAY_BIT_TOTAL) / 2);
+	// Wait for the slave to finish writing the payload
+	delayMicroseconds((HDQ_DELAY_TRSPS_MAX - HDQ_DELAY_BIT_TOTAL) / 2);
 
-cbi(*modeReg, pin);     // Release pin
-
-return true;
-
+	cbi(*modeReg, pin);     // Release pin
 }
 
 /*
@@ -177,14 +176,15 @@ return true;
 * Write with verification 
 *
 */ 
-bool HDQ::write(uint8_t reg, uint8_t payload, bool verif) { // Write the payload 
-HDQ::write(reg, payload);
+bool HDQ::write(uint8_t reg, uint8_t payload, bool verif)
+{
+	HDQ::write(reg, payload);
 
-// Verify the write
-if (payload == HDQ::read(reg)) return true;
-
-return false;
-
+	// Verify the write
+	if (payload == HDQ::read(reg))
+		return true;
+	else
+		return false;
 }
 
 /*
@@ -194,33 +194,33 @@ return false;
 * @return a uint8_t integer 
 *
 */ 
-uint8_t HDQ::read(uint8_t reg) {
-uint8_t result = 0; 
-uint8_t maxTries = HDQ_DELAY_FAIL_TRIES; // ~128uS at 8Mhz with 8 instructions per loop 
+uint8_t HDQ::read(uint8_t reg)
+{
+	uint8_t result = 0; 
+	uint8_t maxTries = HDQ_DELAY_FAIL_TRIES; // ~128uS at 8Mhz with 8 instructions per loop 
 
-// Singal a break
-HDQ::doBreak();
+	// Singal a break
+	HDQ::doBreak();
 
-// Write the register to read
-HDQ::writeByte((reg |= HDQ_ADDR_MASK_READ));
+	// Write the register to read
+	HDQ::writeByte((reg |= HDQ_ADDR_MASK_READ));
 
-for (uint8_t ii = 0; ii < 8; ii++) {
-    // Wait for the slave to toggle a low, or fail
-    maxTries = HDQ_DELAY_FAIL_TRIES;
-	while (_HDQ_readPin() != 0 && maxTries-- > 0)
-		if (maxTries == 1) return 0xFF;
+	for (uint8_t ii = 0; ii < 8; ii++)
+	{
+		// Wait for the slave to toggle a low, or fail
+		maxTries = HDQ_DELAY_FAIL_TRIES;
+		while (_HDQ_readPin() != 0 && maxTries-- > 0)
+			if (maxTries == 1) return 0x05;
 
-    // Wait until Tdsub and half or one bit has passed
-	delayMicroseconds(((HDQ_DELAY_TDW0 - HDQ_DELAY_TDW1) / 2) + HDQ_DELAY_TDW1);
-    // Read the bit
-    result |= _HDQ_readPin()<<ii;
+		// Wait until Tdsub and half or one bit has passed
+		delayMicroseconds(((HDQ_DELAY_TDW0 - HDQ_DELAY_TDW1) / 2) + HDQ_DELAY_TDW1);
+		// Read the bit
+		result |= _HDQ_readPin()<<ii;
 
-    // Wait until Tssub has passed
-	delayMicroseconds(HDQ_DELAY_TCYCD - HDQ_DELAY_TDW0);
-}
+		// Wait until Tssub has passed
+		delayMicroseconds(HDQ_DELAY_TCYCD - HDQ_DELAY_TDW0);
+	}
 
-delayMicroseconds(HDQ_DELAY_TB);
-
-return result;
-
+	delayMicroseconds(HDQ_DELAY_TB);
+	return result;
 }
